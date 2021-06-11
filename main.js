3225553cc8b9670a1e9becf61c2bf9faca7bd31b
VaishnaviSharma0209 <72790064+VaishnaviSharma0209@users.noreply.github.com>
2021-06-11 14:13:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function setup(){
+    canvas=createCanvas(400,400);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.size(400,400)
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses)
+}
+function draw(){
+    background("springgreen")
+}
+function modelLoaded(){
+    console.log("Posenet loaded")
+}
+function gotPoses(results){
+    if (results.length>0){
+        console.log(results);
+    }
+}
